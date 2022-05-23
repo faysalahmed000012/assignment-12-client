8a73e9ab2f99ed511c80a2abf85679669969e6aa
faysalahmed000012 <faysal000012@gmail.com>
2022-05-23 13:21:29 +0600

    header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import { Route, Routes } from "react-router-dom";
 import Blogs from "./Components/Pages/Blogs/Blogs";
 import Home from "./Components/Pages/Home/Home";
+import Login from "./Components/Pages/Login/Login";
 import MyPortfolio from "./Components/Pages/MyPortfolio/MyPortfolio";
 import MyProfile from "./Components/Pages/MyProfile/MyProfile";
 import NotFound from "./Components/Pages/NotFound/NotFound";
+import Signup from "./Components/Pages/Signup/Signup";
+import Header from "./Components/Shared/Header/Header";
 
 function App() {
   return (
     <div>
+      <Header></Header>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/home" element={<Home></Home>}></Route>
         <Route path="/blogs" element={<Blogs></Blogs>}></Route>
         <Route path="/myProfile" element={<MyProfile></MyProfile>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/signup" element={<Signup></Signup>}></Route>
         <Route
           path="/myPortfolio"
           element={<MyPortfolio></MyPortfolio>}
